0966e58f277af6ece9e2bda056d2efa909aa0782
deepraj16 <149596333+deepraj16@users.noreply.github.com>
2025-01-21 13:38:48 +0530

    Delete part-11(postgress) directory

--- part-11(postgress)/config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/bookstore' 
-                                                             #two database 5433,1234
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
